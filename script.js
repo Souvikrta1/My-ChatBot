32f171ad76de7a62ac62a1ffed1b1f48c0ef05f8
Souvikrta1 <souvikblaze1@gmail.com>
2023-03-15 08:34:37 +0530

    server update

--- script.js
+++ script.js
@@ -77,7 +77,7 @@ const handleSubmit = async (e) => {
 
   //fetch data from server
 
-  const response = await fetch('https://chatbot-server-ynng.onrender.com', {
+  const response = await fetch('https://chatbot-server-yuzg.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
